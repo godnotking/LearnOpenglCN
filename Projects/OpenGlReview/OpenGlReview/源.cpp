969b2590b8d685461f83bcfcc1e4760ce8ed791b
945556712 <945556712@qq.com>
2020-03-30 17:38:24 +0800

    添加初始化仓库文件

---
+++ Projects/OpenGlReview/OpenGlReview/源.cpp
@@ -0,0 +1,67 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <iostream>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+
+void process_input(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+
+	}
+
+}
+
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		// 输入
+		process_input(window);
+
+		// 渲染
+		glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// 检查并调用事件， 交换缓冲
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//getchar();
+
+	glfwTerminate();
+	return 0;
+
+}
